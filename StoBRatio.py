b4307ad0db5549998611d9e478a124b14a59a7df
julia1234456 <julia.renard@epfl.ch>
2024-04-26 12:51:19 +0200

    ratio hist

--- StoBRatio.py
+++ StoBRatio.py
@@ -125,6 +125,36 @@ def style_histo_curves(histo_sgn, histo_bkg):
     histo_sgn.SetLineColor(kRed)
     histo_sgn.SetFillColor(kRed - 10)
    
+if __name__ == "__main__":
+
+    # Modify basepath and outfile_path according to your environment.
+    basepath = '/home/julia/TMVA/'
+    outfile_path = '/plotsCompare/Ratio/'
+
+    treename = 'coupl'
+
+    samples = {
+        'sgn': basepath + '2b2vl_sgn_ntuple.root',
+        'bbjj': basepath + '2b2j_bkg_ntuple.root',
+        'ww': basepath + 'ww_bkg_ntuple.root',
+        'zz': basepath + 'zz_bkg_ntuple.root',
+    }
+
+    
+    hists = {
+        'leadpT_Jet_Mass' : [100, 0, 20, ' Mass [GeV]'],
+        'leadpT_Jet': [100, 10, 120, 'pT [GeV]'],
+        'met_Met': [100,0,120, 'MET [GeV]']
+    }
+    
+   
+  
+    file_sgn = ROOT.TFile(samples['sgn'], "READ")
+    tree_sgn = file_sgn.Get(treename)
+    
+    create_hist_ratio(samples, hists, tree_sgn, treename, outfile_path) 
+
+    file_sgn.Close()
 
 
 
